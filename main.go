31eb4e18f9bf700d133056971c7a4989b2b5190c
Kenzō <115029856+iTsLhaj@users.noreply.github.com>
2026-01-01 13:53:29 +0100

    ...

--- main.go
+++ main.go
@@ -1,6 +1,15 @@
 package main
 
 import (
+	"database/sql"
+	"log"
+	"time"
+
+	"github.com/google/uuid"
+	"github.com/iTsLhaj/chirpy/internal/database"
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
+
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
@@ -12,11 +21,23 @@ import (
 
 type apiConfig struct {
 	fileServerHits atomic.Int32
+	dbQuery        *database.Queries
+	platform       string
 }
 
 func (apic *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		apic.fileServerHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (apic *apiConfig) middlewareRouteProtection(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if apic.platform != "dev" {
+			w.WriteHeader(http.StatusForbidden)
+			return
+		}
 		next.ServeHTTP(w, r)
 	})
 }
@@ -43,64 +64,153 @@ func (apic *apiConfig) fetchFileServerHits() http.Handler {
 
 func (apic *apiConfig) resetFileServerHits() http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		err := apic.dbQuery.DeleteAllUsers(r.Context())
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
 		apic.fileServerHits.And(0)
 		w.Header().Set("Server", "Kahya 1.0")
 		w.WriteHeader(http.StatusOK)
 	})
 }
 
-func handleChirpValidation(w http.ResponseWriter, r *http.Request) {
-	type reqJSON struct {
-		Body string `json:"body"`
-	}
-	type errResp struct {
-		Error string `json:"error"`
-	}
-	type validResp struct {
-		CleanedBody string `json:"cleaned_body"`
-	}
-	var reqPostData reqJSON
+func (apic *apiConfig) handleChirpValidation() http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		type reqJSON struct {
+			Body string `json:"body"`
+		}
+		type errResp struct {
+			Error string `json:"error"`
+		}
+		type validResp struct {
+			CleanedBody string `json:"cleaned_body"`
+		}
+		var reqPostData reqJSON
 
-	raw, err := ioutil.ReadAll(r.Body)
+		raw, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			data, _ := json.Marshal(errResp{"Something went wrong"})
+			w.Write(data)
+			return
+		}
+
+		err = json.Unmarshal(raw, &reqPostData)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			data, _ := json.Marshal(errResp{"Something went wrong"})
+			w.Write(data)
+			return
+		}
+
+		if len(reqPostData.Body) > 140 {
+			w.WriteHeader(http.StatusBadRequest)
+			data, _ := json.Marshal(errResp{"Chirp is too long"})
+			w.Write(data)
+			return
+		}
+
+		var cleaned []byte = []byte(reqPostData.Body)
+		re, _ := regexp.Compile(`(?i)\bFornax|Kerfuffle|Sharbert\b`)
+		cleaned = re.ReplaceAll([]byte(cleaned), []byte("****"))
+
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		data, _ := json.Marshal(validResp{CleanedBody: string(cleaned)})
+		w.Write(data)
+	})
+}
+
+func (apic *apiConfig) handleUserCreation() http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		type reqJSON struct {
+			Email string `json:"email"`
+		}
+		type errResp struct {
+			Error string `json:"error"`
+		}
+		type User struct {
+			Id        uuid.UUID `json:"id"`
+			CreatedAt time.Time `json:"created_at"`
+			UpdatedAt time.Time `json:"updated_at"`
+			Email     string    `json:"email"`
+		}
+
+		var reqPostData reqJSON
+		var raw []byte
+		var err error
+		raw, err = ioutil.ReadAll(r.Body)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			data, _ := json.Marshal(errResp{"Something went wrong"})
+			w.Write(data)
+			return
+		}
+
+		err = json.Unmarshal(raw, &reqPostData)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			data, _ := json.Marshal(errResp{"Something went wrong"})
+			w.Write(data)
+			return
+		}
+
+		email := reqPostData.Email
+		var dbUser database.User
+		dbUser, err = apic.dbQuery.CreateUser(r.Context(), email)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			data, _ := json.Marshal(errResp{"Something went wrong"})
+			w.Write(data)
+			return
+		}
+
+		var user User
+		user.Id = dbUser.ID
+		user.CreatedAt = dbUser.CreatedAt
+		user.UpdatedAt = dbUser.UpdatedAt
+		user.Email = dbUser.Email
+		var respBody []byte
+		respBody, err = json.Marshal(user)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			data, _ := json.Marshal(errResp{"Something went wrong"})
+			w.Write(data)
+			return
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusCreated)
+		w.Write(respBody)
+	})
+}
+
+func loadDotEnv() {
+	err := godotenv.Load()
 	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		data, _ := json.Marshal(errResp{"Something went wrong"})
-		w.Write(data)
-		return
+		log.Fatal("Error loading .env file")
 	}
-
-	err = json.Unmarshal(raw, &reqPostData)
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		data, _ := json.Marshal(errResp{"Something went wrong"})
-		w.Write(data)
-		return
-	}
-
-	if len(reqPostData.Body) > 140 {
-		w.WriteHeader(http.StatusBadRequest)
-		data, _ := json.Marshal(errResp{"Chirp is too long"})
-		w.Write(data)
-		return
-	}
-
-	var cleaned []byte = []byte(reqPostData.Body)
-	re, _ := regexp.Compile(`(?i)\bFornax|Kerfuffle|Sharbert\b`)
-	cleaned = re.ReplaceAll([]byte(cleaned), []byte("****"))
-
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
-	data, _ := json.Marshal(validResp{CleanedBody: string(cleaned)})
-	w.Write(data)
 }
 
 func main() {
+	loadDotEnv()
+	dbURL := os.Getenv("DB_URL")
+	db, err := sql.Open("postgres", dbURL)
+	if err != nil {
+		log.Fatal(err)
+	}
+	dbQueries := database.New(db)
+
 	var sm *http.ServeMux = http.NewServeMux()
 	var server http.Server = http.Server{
 		Addr:    ":8080",
 		Handler: sm,
 	}
-	var apic apiConfig = apiConfig{}
+	var apic apiConfig = apiConfig{
+		dbQuery:  dbQueries,
+		platform: os.Getenv("PLATFORM"),
+	}
 
 	sm.Handle("/app/", apic.middlewareMetricsInc(
 		http.StripPrefix("/app", http.FileServer(http.Dir(".")))))
@@ -115,13 +225,14 @@ func main() {
 			w.WriteHeader(http.StatusInternalServerError)
 		}
 	})
-	sm.HandleFunc("POST /api/validate_chirp", handleChirpValidation)
+	sm.Handle("POST /api/validate_chirp", apic.handleChirpValidation())
+	sm.Handle("POST /api/users", apic.handleUserCreation())
 
-	sm.Handle("GET /admin/metrics", apic.fetchFileServerHits())
-	sm.Handle("POST /admin/reset", apic.resetFileServerHits())
+	sm.Handle("GET /admin/metrics", apic.middlewareRouteProtection(apic.fetchFileServerHits()))
+	sm.Handle("POST /admin/reset", apic.middlewareRouteProtection(apic.resetFileServerHits()))
 
 	fmt.Printf("Listening on port %s\n", server.Addr)
-	err := server.ListenAndServe()
+	err = server.ListenAndServe()
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
---
+++ sql/schema/001_users.sql
@@ -0,0 +1,12 @@
+-- +goose Up
+CREATE TABLE users (
+    id uuid PRIMARY KEY,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP NOT NULL,
+    email TEXT NOT NULL,
+
+    UNIQUE (email)
+);
+
+-- +goose Down
+DROP TABLE users;
